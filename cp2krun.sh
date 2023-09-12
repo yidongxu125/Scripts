b338769c60aac797cdf9d4f69127e0aba08b692e
Fishman <102980937+yidongxu125@users.noreply.github.com>
2023-09-12 17:18:50 +0800

    doit
    do

--- cp2krun.sh
+++
@@ -1,5 +0,0 @@
-# cp2k allrun 
-# Version 1.0
-# By Yidongxu
-mpirun -np 35 cp2k.popt formA_nodisorder.inp |tee formA_nodisorder.out
-mpirun -np 35 cp2k.popt formD_nodisorder.inp |tee formD_nodisorder.out
